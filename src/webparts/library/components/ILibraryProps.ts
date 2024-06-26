aded4f20d5393f45d84892e701244cf505be023f
Vignesh R <98761344+vickyportfolio@users.noreply.github.com>
2024-06-26 17:12:03 +0530

    Delete src directory

--- src/webparts/library/components/ILibraryProps.ts
+++
@@ -1,7 +0,0 @@
-export interface ILibraryProps {
-  description: string;
-  // isDarkTheme: boolean;
-  // environmentMessage: string;
-  // hasTeamsContext: boolean;
-  // userDisplayName: string;
-}
